2bb97b540d0526fd2c66cceaa1409ecfc66f0888
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2019-12-03 07:33:22 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_long_description() -> str:
 setup(
     name="postgresimporter",
     packages=["postgresimporter"],
-    version="0.1.0",
+    version="0.1.1",
     license="MIT",
     description=(
         "A simple python wrapper script based on pgfutter "
